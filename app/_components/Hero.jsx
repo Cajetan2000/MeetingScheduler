6b644a6c8a2b82ccda87eca2f3b924c274e49b8f
Cajetan Songwae <92754440+Cajetan2000@users.noreply.github.com>
2024-04-12 10:58:11 +0000

    CHANGED NAME

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -18,7 +18,7 @@ function Hero() {
         </div>
         <div className='text-center max-w-3xl'>
         <h2 className='font-bold text-[60px] text-slate-700'>Easy scheduling ahead</h2>
-        <h2 className='text-xl mt-5 text-slate-500'>Scheduly is your scheduling automation platform for eliminating the back-and-forth emails to find the perfect time — and so much more.</h2>
+        <h2 className='text-xl mt-5 text-slate-500'>A-Shesi Schedule is your scheduling automation platform for eliminating the back-and-forth emails to find the perfect time — and so much more.</h2>
         <div className='flex gap-4 flex-col mt-5'>
             <h3 className='text-sm'>Sign Up free with Google and Facebook</h3>
             <div className='flex justify-center gap-8'>
